f488aaff9dcfadc293fc59052fa5e02d300ec866
Joakim G.L <joakigl@stud.ntnu.no>
2017-03-14 19:22:06 +0100

    Thomas is a moron, he replies "AIDS..."

--- airbnb.py
+++ airbnb.py
@@ -1,7 +1,9 @@
 from pyspark import SparkContext, SparkConf
 from pyspark import SQLContext
+from pyspark.sql import functions
 from collections import OrderedDict
 import sys
+#import org.apache.spark.sql.functions.{lower, upper}
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -10,10 +12,16 @@ conf = (SparkConf().setMaster("local[*]").setAppName("AirBnB Application"))
 sc = SparkContext(conf=conf)
 sqlContext = SQLContext(sc)
 
-calendarDF = sqlContext.read.csv("C:/Users/Thomas/Desktop/airbnb_datasets_old/calendar_us.csv", sep="\t", header=True)
-listingsDF = sqlContext.read.csv("C:/Users/Thomas/Desktop/airbnb_datasets_old/listings_us.csv", sep="\t", header=True)
-neighbourhoodsDF = sqlContext.read.json("C:/Users/Thomas/Desktop/airbnb_datasets_old/neighbourhoods.geojson")
-reviewsDF = sqlContext.read.csv("C:/Users/Thomas/Desktop/airbnb_datasets_old/reviews_us.csv", sep="\t", header=True)
+calLoc = "airbnb_datasets/calendar_us.csv"
+lisLoc = "airbnb_datasets/listings_us.csv"
+neighLoc = "airbnb_datasets/neighbourhoods.geojson"
+revLoc = "airbnb_datasets/reviews_us.csv"
+
+calendarDF = sqlContext.read.csv(calLoc, sep="\t", header=True)
+listingsDF = sqlContext.read.csv(lisLoc, sep="\t", header=True)
+neighbourhoodsDF = sqlContext.read.json(neighLoc)
+reviewsDF = sqlContext.read.csv(revLoc, sep="\t", header=True)
+listings = sc.textFile(lisLoc)
 
 listingsDF.printSchema()
 
@@ -24,11 +32,17 @@ listingsDF.printSchema()
 neighbourhoodsDF.printSchema()
 reviewsDF.printSchema()
 """
-"""
+
 #2. b)
+"""counts = None
+for column in listingsDF.schema.names:
+    counts.union(listingsDF.select(column).na.drop().rdd.map(lambda x: (column,str(x).upper())).distinct().count())
+    
+counts.saveAsTextFile("distinctCounts")"""
+
 distinctValuesPerColumn = OrderedDict()
 for column in listingsDF.schema.names:
-	distinctValuesPerColumn[column] = listingsDF.select(column).distinct().count()
+	distinctValuesPerColumn[column] = listingsDF.select(column).na.drop().rdd.map(lambda x: str(x).upper()).distinct().count()
 
 print "Distinct values per field:"
 print distinctValuesPerColumn
@@ -39,15 +53,30 @@ output = open("output.txt", "w")
 for key in distinctValuesPerColumn:
 	output.write(str(key) + "\t" + str(distinctValuesPerColumn[key]) + "\n")
 output.close()
-"""
+
 
 #2. c)
-cities = listingsDF.select("city").distinct()
-"""
-output = open("output.txt", "w")
-for city in cities:
-	output.write(str(city) + ", ")
-output.close()
-"""
+#cities = listingsDF.select("city").distinct()
+#cities.rdd.map(lambda p: unicode(p[0])).coalesce(1).saveAsTextFile("citiesResults")
 
-#listingsDF.where(listingsDF.id == "12607303").select("city", "country", "country_code", "host_location", "is_location_exact", "latitude", "longitude", "neighborhood_overview", "region_id", "region_name", "region_parent_id", "state", "street", "zipcode").show()
+
+#listingsDF.select("market").distinct().show()
+
+#3. a)
+#cities = listingsDF.select("city","region_name","smart_location","state","street").distinct().collect()
+#cities = listingsDF.select("state").rdd.flatMap(lambda x: x).distinct().collect()
+#cities = listingsDF.select("smart_location").rdd.flatMap(lambda x: x).distinct().collect()
+
+#cities = listingsDF.select("state").rdd.map(lambda x: str(x.state).upper()).distinct().collect()
+#cities = listingsDF.na.drop(subset=["state"]).select("state").rdd.map(lambda x: str(x.state).upper()).distinct().collect()
+
+#cities = listingsDF.select("state").na.drop().rdd.map(lambda x: str(x.state).upper()).distinct().collect()
+
+#cities = listingsDF.select("state").na.drop().rdd.map(lambda x: "NY" if str(x.state).upper()=="NEW YORK" else str(x.state).upper()).distinct().collect()
+#cities = cities.withColumn("state", functions.when(cities["state"]=="NEW YORK", "NY").otherwise(cities["state"])).distinct().collect()
+
+#for c in cities:
+#    print c
+#pricePerCity = listingsDF.rdd.map(lambda c: for 
+
+
