6e2a9b311519ba0c260dc4d066938ab8744cfbcd
Thomas <thomas.desember29@gmail.com>
2017-03-16 19:32:46 +0100

    Added task 5. a) and minor cleanup
    Added task 5. a) and minor cleanup

--- airbnb.py
+++ airbnb.py
@@ -1,11 +1,7 @@
 from pyspark import SparkContext, SparkConf
 from pyspark import SQLContext
 from collections import OrderedDict
-from pyspark.sql import Column
-from pyspark.sql.functions import avg
-from pyspark.sql.types import StructType
 import sys
-#import org.apache.spark.sql.functions.{lower, upper}
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -23,32 +19,19 @@ calendarDF = sqlContext.read.csv(calLoc, sep="\t", header=True)
 listingsDF = sqlContext.read.csv(lisLoc, sep="\t", header=True)
 neighbourhoodsDF = sqlContext.read.json(neighLoc)
 reviewsDF = sqlContext.read.csv(revLoc, sep="\t", header=True)
-#listings = sc.textFile(lisLoc)
 
-"""
+
 #To print schemas
+"""
 calendarDF.printSchema()
 listingsDF.printSchema()
 neighbourhoodsDF.printSchema()
 reviewsDF.printSchema()
 """
-"""
+
+
 #2. b)
-#distinctValueCount = listingsDF.rdd.map(lambda listing: ((column for column in listingsDF.schema.names), (listing.Column(column for column in listingsDF.schema.names))))
-
-#distinctValueCount = listingsDF.rdd.map(lambda listing: ((column for column in listingsDF.schema.names), ((column for column in listingsDF.schema.names).distinct().count()))).take(5)
-#for column in listingsDF.schema.names:
-#    counts.union(listingsDF.select(column).na.drop().rdd.map(lambda x: (column,str(x).upper())).distinct().count())
-
-#for column in listingsDF.schema.names:
-#	result = listingsDF.rdd.map(lambda row: (column, (row.Column(column).distinct().count())))
-#	result.take(5)
-	
-
-#counts.saveAsTextFile("distinctCounts")
-
-
-#Temporary fix to problem, attempting to figure out way to 
+"""
 distinctValuesPerColumn = OrderedDict()
 for column in listingsDF.schema.names:
 	distinctValuesPerColumn[column] = listingsDF.select(column).na.drop().rdd.map(lambda x: str(x).upper()).distinct().count()
@@ -60,21 +43,31 @@ for key in distinctValuesPerColumn:
 	output.write(str(key) + "\t" + str(distinctValuesPerColumn[key]) + "\n")
 output.close()
 """
-"""
+
 #2. c)
+"""
 cities = listingsDF.select("city").distinct()
 cities.rdd.map(lambda p: unicode(p[0])).coalesce(1).saveAsTextFile("citiesResults")
+
+#On the AirBnB old dataset this was our solution, as the cities were not normalized and were usually spelled wrongly. So we thought to extract state from the listings since state had less wrong entries and required little cleaning, then convert state (was on short form "NY") to the biggest city in the State. Then use this as the listings city. We could've also done this or something similar in the other tasks, but when we found the new normalized datasets we instead chose to simplify our work amount by using the new datasets.
+'''
+#This import needs to be added as we (intended) to use it to convert the strings to lower/upper.
+import org.apache.spark.sql.functions.{lower, upper}
+listingsDF = sqlContext.read.csv("../airbnb_datasets_old/listings_us.csv", sep="\t", header=True)
+cities = listingsDF.select("state").na.drop().rdd.map(lambda x: "NY" if str(x.state).upper()=="NEW YORK" else str(x.state).upper()).distinct().collect()
+print cities
+'''
 """
 
-"""
 #3. a)
+"""
 priceAveragePerCityRDD = listingsDF.select("city", "price").rdd.map(lambda listing: (listing.city, float("".join(c for c in listing.price if c not in "$,"))))
 priceAveragePerCityRDD = priceAveragePerCityRDD.aggregateByKey((0, 0), lambda city, price: (city[0] + price, city[1] + 1), lambda city, price: (city[0] + price[0], city[1] + price[1]))
-priceAveragePerCityRDD = priceAveragePerCityRDD.mapValues(lambda row: row[0]/row[1]).collect()
+priceAveragePerCityRDD = priceAveragePerCityRDD.mapValues(lambda row: row[0] / row[1]).collect()
 print priceAveragePerCityRDD
-"""
-"""
+
 #Verification function to find average price in New York, only for testing/debugging of code above
+'''
 pricesForNewYork = listingsDF.where(listingsDF.city == "New York").select("price").rdd.map(lambda listing: float("".join(c for c in listing.price if c not in "$,"))).collect()
 counter = 0
 totalPrice = 0
@@ -84,53 +77,58 @@ for item in pricesForNewYork:
 
 averageForNewYork = totalPrice/counter
 print "Average price for New York is " + str(averageForNewYork)
+'''
 """
 
-"""
 #3. b)
+"""
 priceAveragePerRoomInCityRDD = listingsDF.select("city", "room_type", "price").rdd.map(lambda listing: ((listing.city, listing.room_type), float("".join(c for c in listing.price if c not in "$,"))))
 priceAveragePerRoomInCityRDD = priceAveragePerRoomInCityRDD.aggregateByKey((0,0),lambda a,b: (a[0]+b,a[1]+1),lambda a,b: (a[0]+b[0],a[1]+b[1]))
 priceAveragePerRoomInCityRDD = priceAveragePerRoomInCityRDD.mapValues(lambda row: row[0]/row[1]).collect()
 print priceAveragePerRoomInCityRDD
 """
-"""
+
 #3. c)
+"""
 reviewAveragePerCityRDD = listingsDF.select("city", "reviews_per_month").rdd.map(lambda listing: (listing.city, 0 if listing.reviews_per_month==None else float("".join(c for c in listing.reviews_per_month if c not in "$,"))))
 reviewAveragePerCityRDD = reviewAveragePerCityRDD.aggregateByKey((0, 0), lambda city, revPerMonth: (city[0] + revPerMonth, city[1] + 1), lambda city, revPerMonth: (city[0] + revPerMonth[0], city[1] + revPerMonth[1]))
 reviewAveragePerCityRDD = reviewAveragePerCityRDD.mapValues(lambda row: row[0]/row[1]).collect()
 print reviewAveragePerCityRDD
 """
-"""
+
 #3. d)
+"""
 numberOfNightsBookedPerYearRDD = listingsDF.select("city", "reviews_per_month").rdd.map(lambda listing: (listing.city, (float(0 if listing.reviews_per_month == None else listing.reviews_per_month) / 0.7) * 3 * 12))
 numberOfNightsBookedPerYearRDD = numberOfNightsBookedPerYearRDD.aggregateByKey((0, 0), lambda city, booking: (city[0] + booking, city[1] + 1), lambda city, booking: (city[0] + booking[0], city[1] + booking[1]))
 numberOfNightsBookedPerYearRDD = numberOfNightsBookedPerYearRDD.mapValues(lambda row: row[0]/row[1]).collect()
 print numberOfNightsBookedPerYearRDD
+"""
 
 #3. e)
+"""
 totalPricePerYearRDD = listingsDF.select("city", "reviews_per_month", "price").rdd.map(lambda listing: (listing.city, (float(0 if listing.reviews_per_month == None else listing.reviews_per_month) / 0.7) * 3 * 12 * float("".join(c for c in listing.price if c not in "$,"))))
 totalPricePerYearRDD = totalPricePerYearRDD.reduceByKey(lambda x, y: x + y).collect()
 print totalPricePerYearRDD
 """
 
 
-"""
 #4. a)
+"""
 totalListings = float(listingsDF.select("id").distinct().count())
 totalHosts = float(listingsDF.select("host_id").distinct().count())
 averageListingsPerHost = float(totalListings/totalHosts)
 print averageListingsPerHost
 """
 
-"""
 #4. b)
+"""
 listForPercentage = listingsDF.select("host_id","host_listings_count").rdd
 percentageOfHostsWithMultipleListings = float(listForPercentage.map(lambda x: (x.host_id,0) if x.host_listings_count==None else (x.host_id,x.host_listings_count)).filter(lambda x: float(x[1]) >= 2).count())/float(listForPercentage.distinct().count())
 print percentageOfHostsWithMultipleListings
 """
 
-"""
 #4. c)
+"""
 listingsTable = listingsDF.select("id","city","host_id","price")
 calendarTable = calendarDF.select("listing_id","date","available").rdd.filter(lambda x: x.available=="f").map(lambda x: (x[0],x[1])).countByKey()
 print calendarTable
@@ -145,29 +143,25 @@ topHostIncome = listingsTable.join(calendarTable, listingsTable.id == calendarTa
 #calendarDF.printSchema()
 #print calendarDF.select("date","available").take(20)
 """
-"""
+
+
 #5. a)
-topGuestsRDD = reviewsDF.join(listingsDF, reviewsDF.listing_id == listingsDF.id).select("city", "reviewer_id").rdd.map(lambda row: (row.city, int(row.reviewer_id)))
-#topGuestsRDD = topGuestsRDD.aggregateByKey((0, 0), lambda x, y: (x[0] + y, x[1] + 1), lambda x, y: (x[0] + y[0], x[1] + y[1])).collect()
-
-print topGuestsRDD
-
-#.where(reviewsDF.reviewer_id == "7107853")
 """
+topGuestsRDD = reviewsDF.join(listingsDF, reviewsDF.listing_id == listingsDF.id).select("city", "reviewer_id").rdd.map(lambda row: ((row.city, int(row.reviewer_id)), 1)).reduceByKey(lambda x, y: x + y).map(lambda x: (x[0][0], (x[0][1], x[1]))).sortBy(lambda x: -x[1][1]).groupByKey().mapValues(list).collect()
+
+topGuestsByCity = OrderedDict()
+
+for city in topGuestsRDD:
+	for n in range(3):
+		if city[0] in topGuestsByCity:
+			topGuestsByCity[city[0]].append(city[1][n])
+		else:
+			topGuestsByCity[city[0]] = [city[1][n]]
+
+print topGuestsByCity
 """
-cities = listingsDF.select("city","region_name","smart_location","state","street").distinct().collect()
-cities = listingsDF.select("state").rdd.flatMap(lambda x: x).distinct().collect()
-cities = listingsDF.select("smart_location").rdd.flatMap(lambda x: x).distinct().collect()
 
-cities = listingsDF.select("state").rdd.map(lambda x: str(x.state).upper()).distinct().collect()
-cities = listingsDF.na.drop(subset=["state"]).select("state").rdd.map(lambda x: str(x.state).upper()).distinct().collect()
+#5. b)
+"""
 
-cities = listingsDF.select("state").na.drop().rdd.map(lambda x: str(x.state).upper()).distinct().collect()
-
-cities = listingsDF.select("state").na.drop().rdd.map(lambda x: "NY" if str(x.state).upper()=="NEW YORK" else str(x.state).upper()).distinct().collect()
-cities = cities.withColumn("state", functions.when(cities["state"]=="NEW YORK", "NY").otherwise(cities["state"])).distinct().collect()
-
-for c in cities:
-    print c
-pricePerCity = listingsDF.rdd.map(lambda c: for 
 """
