78707c3018342bdc8ddcc16dd74d8068ac5d2b6b
Thomas <thomas.desember29@gmail.com>
2017-03-24 22:07:15 +0100

    Loaded in the files and created new listingsDF
    Loaded in the files and created new listingsDF

--- Assignment2/tf_idf.py
+++ Assignment2/tf_idf.py
@@ -1,13 +1,40 @@
 """
-Try to run the file with command: spark-submit tf-idf.py <arguments>.
+Try to run the file with command: spark-submit tf_idf.py <arguments>.
 For more info, visit: http://spark.apache.org/docs/latest/quick-start.html#self-contained-applications.
 """
 
-from pyspark import SparkContext
+from pyspark import SparkContext, SQLContext
+from pyspark.sql.types import StringType, StructField
 import sys
+
+#Needed in order to remove an error message when trying to print results. Something about utf-8/ascii encoding error or the like.
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 sc = SparkContext("local", "TF-IDF")
 sc.setLogLevel("ERROR")
+sqlContext = SQLContext(sc)
+
+#Relative path for datasets.
+#calendarFileLocation = "../airbnb_datasets/calendar_us.csv"
+listingsFileLocation = "../airbnb_datasets/listings_us.csv"
+listingsWithNeighbourhoodsFileLocation = "../airbnb_datasets/listings_ids_with_neighborhoods.tsv"
+#neighbourhoodTestFileLocation = "../airbnb_datasets/neighborhood_test.csv"
+#neighhoodsFileLocaction = "../airbnb_datasets/neighbourhoods.geojson"
+#reviewsFileLocation = "../airbnb_datasets/reviews_us.csv"
+
+#Read the datasets as dataframes that are tab seperated and with headers.
+listingsDF = sqlContext.read.csv(listingsFileLocation, sep="\t", header=True)
+listingsWithNeighbourhoodsDF = sqlContext.read.csv(listingsWithNeighbourhoodsFileLocation, sep="\t", header=True)
+
+#Generate new listings dataframe with neighbourhoods added.
+listingsDF = listingsDF.join(listingsWithNeighbourhoodsDF, "id")
+
+#To print schemas
+"""
+listingsDF.printSchema()
+listingsWithNeighbourhoodsDF.printSchema()
+"""
 
 print("TF-IDF Assignment")
 file = sc.textFile("data.txt").cache()
